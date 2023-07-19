afb002d4b8d9d0999028578f1cd03a0d6bc0fef3
Reytov <Lorak1301@gmail.com>
2023-07-19 13:41:28 +0200

    division into modules

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = require('./modules/tabs'),
+          modal = require('./modules/modal'),
+          calc = require('./modules/calc'),
+          cards = require('./modules/cards'),
+          forms = require('./modules/forms'),
+          slider = require('./modules/slider'),
+          timer = require('./modules/timer');
+
+    tabs();
+    modal();
+    calc();
+    cards();
+    forms();
+    slider();
+    timer();
+
+});
